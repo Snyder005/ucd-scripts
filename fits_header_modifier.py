697b151a34fc08a11b3976fc1697db238fc5a09b
CCD Lab <ccd@viscacha.physics.ucdavis.edu>
2023-05-10 11:32:47 -0700

    Made changes to the fits header modifier to correct files in place.

--- fits_header_modifier.py
+++ fits_header_modifier.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
 from astropy.io import fits
 import argparse
+import copy
 
 def main(infiles):
 
     for infile in infiles:
         print("Opening {0}".format(infile))
         
-        with fits.open(infile) as hdulist:
+        with fits.open(infile, mode='update') as hdulist:
         
+            ## Check file history
+            if 'HISTORY' in hdulist[0].header:
+                print("Amps already reordered! Skipping...")
+                continue
+
             ## Rearrange HDUs
             for i in range(9, 13):
                 
-                data1 = hdulist[i].data
-                data2 = hdulist[25-i].data
+                data1 = copy.deepcopy(hdulist[i].data)
+                data2 = copy.deepcopy(hdulist[25-i].data)
                 
                 hdulist[i].data = data2
                 hdulist[25-i].data = data1
+
+            hdulist[0].header.add_history('Amp Order Fixed.')
+            hdulist.flush()
+            print("Amp order fixed.")
 
 if __name__ == '__main__':
 
