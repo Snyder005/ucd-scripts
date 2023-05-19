360676480b923852cf354233a2991b7a2d571de1
CCD Lab <ccd@viscacha.physics.ucdavis.edu>
2023-05-19 12:58:09 -0700

    Finished power on/off scripts.

--- ucdInit.py
+++ ucdInit.py
@@ -2,12 +2,21 @@
 from org.lsst.ccs.scripting import CCS
 from java.time import Duration
 
+CCS.setThrowExceptions(True)
 fp = CCS.attachSubsystem("ucd-fp")
 
-## Check state of back bias voltage
+## Turn off back bias switch
 hvSwitchOn = fp.sendSynchCommand("R22/Reb0 isBackBiasOn")
 if hvSwitchOn:
-    raise RuntimeError("HVBias must be off to run this script.")
+    print "Setting back bias switch off."
+    fp.setSynchCommand("R22/Reb0 setBackBias false")
+
+## Check state of focal plane
+ccdState = fp.sendSynchCommand("R22/Reb0 getCCDsPowerState")
+if ccdState == 'OFF' and hvSwitchOn:
+    raise RuntimeError("CCD is powered off but back bias switch was on!")
+elif ccdState == 'ON':
+    raise RuntimeError("CCD is already power on.")
 
 ## List REB voltages
 print "RefP12 = {0:.1f} Volts (Positive 12 ref)".format(fp.sendSynchCommand("R22/Reb0/RefP12 getValue"))
@@ -16,11 +25,6 @@ print "Ref05V = {0:.1f} Volts (Positive 12 ref)".format(fp.sendSynchCommand("R22
 print "Ref15V = {0:.1f} Volts (Positive 12 ref)".format(fp.sendSynchCommand("R22/Reb0/Ref15V getValue"))
 print "Ref25V = {0:.1f} Volts (Positive 12 ref)".format(fp.sendSynchCommand("R22/Reb0/Ref25V getValue"))
 print "Ref125V = {0:.1f} Volts (Positive 12 ref)".format(fp.sendSynchCommand("R22/Reb0/Ref125V getValue"))
-
-## Check state of CCDs
-ccdState = fp.sendSynchCommand("R22/Reb0 getCCDsPowerState")
-if ccdState == 'OFF':
-    raise RuntimeError("CCDs must be off to run this script.")
 
 ## Modify CCD voltage limits
 ccdTypes = fp.sendSynchCommand("getCCDType")
@@ -36,10 +40,12 @@ if ccdType == 'itl':
 
 ## Power CCDs On
 fp.sendSynchCommand(Duration.ofSeconds(300), "R22/Reb0 powerCCDsOn")
-print "CCDs power on."
-
-## List REB voltages
-print "ODV = {0:.1f} Volts".format(fp.sendSynchCommand("R22/Reb0/S00/ODV getValue"))
-print "OGV = {0:.1f} Volts".format(fp.sendSynchCommand("R22/Reb0/S00/OGV getValue"))
-print "RDV = {0:.1f} Volts".format(fp.sendSynchCommand("R22/Reb0/S00/RDV getValue"))
-print "GDV = {0:.1f} Volts".format(fp.sendSynchCommand("R22/Reb0/S00/GDV getValue"))
+ccdState = fp.sendSynchCommand("R22/Reb0 getCCDsPowerState")
+if ccdState == 'OFF':
+    raise RuntimeError("CCD failed to power on.")
+else:
+    print "CCD power {0}.".format(ccdState.lower())
+    print "ODV = {0:.1f} Volts".format(fp.sendSynchCommand("R22/Reb0/S00/ODV getValue"))
+    print "OGV = {0:.1f} Volts".format(fp.sendSynchCommand("R22/Reb0/S00/OGV getValue"))
+    print "RDV = {0:.1f} Volts".format(fp.sendSynchCommand("R22/Reb0/S00/RDV getValue"))
+    print "GDV = {0:.1f} Volts".format(fp.sendSynchCommand("R22/Reb0/S00/GDV getValue"))
--- ucdOff.py
+++ ucdOff.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env ccs-script
 from org.lsst.ccs.scripting import CCS
 
+CCS.setThrowExceptions(True)
 fp = CCS.attachSubsystem("ucd-fp")
 
-ccdState = 
-
-## Check initial state
+## Turn off back bias relay
 hvSwitchOn = fp.sendSynchCommand("R22/Reb0 isBackBiasOn")
 if hvSwitchOn:
     print "Setting back bias switch off."
     fp.setSynchCommand("R22/Reb0 setBackBias false")
 
+## Check state of focal plane
+ccdState = fp.sendSynchCommand("R22/Reb0 getCCDsPowerState")
+if ccdState == 'OFF' and hvSwitchOn:
+    raise RuntimeError("CCD is powered off but back bias switch was on!")
+elif ccdState == 'OFF' and not hvSwitchOn:
+    raise RuntimeError("CCD is already powered off.")
+
+## Power CCD off
 fp.sendSynchCommand("R22/Reb0 powerCCDsOff")
+ccdState = fp.sendSynchCommand("R22/Reb0 getCCDsPowerState")
+if ccdState == 'ON':
+    raise RuntimeError("CCD failed to power off.")
+else:
+    print "CCD power {0}.".format(ccdState.lower())
