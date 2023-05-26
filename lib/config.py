53ea2b8848168a44f59571257eea93014b12387c
Adam Snyder <aksnyder@phys-ps514a-mac.physics.ucdavis.edu>
2023-05-26 15:10:59 -0700

    Added logging to config parsing for human-readable output to log file.

--- lib/config.py
+++ lib/config.py
@@ -2,20 +2,59 @@ import re
 import ConfigParser
 import StringIO
 import acquire
+import logging
+
+logger = logging.getLogger(__name__)
 
 def parseConfig(filename):
+    """Parse a data acquisition configuration file.
+
+    Parameters
+    ----------
+    filename : `str`
+        Configuration file name.
+    
+    Returns
+    -------
+    config : `ConfigParser.SafeConfigParser`
+        Configuration parser object.
+    """
     with open(filename) as f:
         lines = f.readlines()
 
     ## Eliminate inline # delimited comments
     slines = map(lambda l: re.sub(r"([^#]*)\s#.*", r"\1", l), lines)
+
     config = ConfigParser.SafeConfigParser(allow_no_value=True)
     config.readfp(StringIO.StringIO("".join(slines)))
+
     return config
 
 def execute(config, command_line_options):
+    """Execute acquisition functions based on configuration.
 
+    Parameters
+    ----------
+    config : `ConfigParser.SafeConfigParser`
+        Configuration parser object.
+    command_line_options: `dict` [`str, `str`]
+        Additional command line options for execution:
+
+        ``"run"``
+            Run number (`str`).
+    """
+    ## Get optional data acquisition description
+    if config.has_section("DESCRIPTION"):
+        description = " ".join(config.options("DESCRIPTION"))
+    else:
+        description = None
+    logger.info("Acquisition Description: {0}".format(description))
+    logger.info("Run Number: {0}".format(command_line_options.get("run", default=None)))
+
+    ## Get data acquisition items
     items = config.options("ACQUIRE")
+    logger.info("Test Types: {0}".format(", ".join(items)))
+
     for item in items:
         options = Config(dict(config.items(item.upper())))
         options.update(command_line_options)
