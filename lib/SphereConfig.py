230b0aab8d5957a3368eb43dce7b88dfdf20a465
CCD Lab <ccd@viscacha.physics.ucdavis.edu>
2023-05-30 10:01:26 -0700

    Changed SphereConfig to snake case.

--- lib/SphereConfig.py
+++ lib/SphereConfig.py
@@ -10,7 +10,9 @@
 import numpy, time, sys, serial, socket
 
 class Sphere(object):
+
     def __init__(self):
+
         socket.setdefaulttimeout(0.5)# Timeout if no connection after 0.5 seconds
         #*** Light & photodiode settings ***
         self.light_IP = '192.168.1.100'
@@ -35,7 +37,7 @@ class Sphere(object):
                          
         return
 
-    def Initialize_Light_Socket(self):
+    def initialize_light_socket(self):
         """ Initializes the light socket over Ethernet"""
         for NumTries in range(3):
             try:
@@ -50,7 +52,7 @@ class Sphere(object):
         print("Failed to initialize Light Socket\n")
         return
 
-    def Close_Light_Socket(self):
+    def close_light_socket(self):
         """ Closes the light socket over Ethernet"""
         try:
             self.light_socket.close()
@@ -60,7 +62,7 @@ class Sphere(object):
             print("Failure to close light socket. Exception of type %s and args = \n"%type(e).__name__, e.args)    
             return
     
-    def Initialize_Shutter_Socket(self):
+    def initialize_shutter_socket(self):
         # Initializes the shutter socket over Ethernet
         # And sets the parameters (speed, acceleration, etc) to the proper values
         for NumTries in range(3):
@@ -94,7 +96,7 @@ class Sphere(object):
         print("Failed to initialize Shutter Socket\n")
         return
 
-    def Close_Shutter_Socket(self):
+    def close_shutter_socket(self):
         """ Closes the shutter socket over Ethernet"""
         try:
             self.shutter_socket.close()
@@ -104,7 +106,7 @@ class Sphere(object):
             print("Failure to close shutter socket. Exception of type %s and args = \n"%type(e).__name__, e.args)    
             return
 
-    def Check_Communications(self):
+    def check_communications(self):
         # Checks on communications staus
         self.light_socket_status = False
         try:
@@ -126,29 +128,29 @@ class Sphere(object):
         return
 
 
-    def Turn_Light_On(self):
+    def turn_light_on(self):
         # Turns the light on
         print("Turning light on - 4 second delay for stabilization.")
         self.light_socket.send(bytes("PS2~1\r", encoding='utf8'))
         time.sleep(4)
-        self.Read_Photodiode()
+        self.read_photodiode()
         return
 
-    def Turn_Light_Off(self):
+    def turn_light_off(self):
         # Turns the light off
         print("Turning light off - 8 second delay for stabilization.")
         self.light_socket.send(bytes("PS2~0\r", encoding='utf8'))
         time.sleep(8)
-        self.Read_Photodiode()
+        self.read_photodiode()
         return
 
-    def Final_Light_Off(self):
+    def final_light_off(self):
         # Turns the light off at program exit without updating photodiode current
         print("Turning light off")
         self.light_socket.send("PS2~0\r")
         return
 
-    def VA_Drive_Shutter(self, Move_value):
+    def va_drive_shutter(self, Move_value):
         # Moves the shutter to a value specified by a Move_value
         # Move_value is a number of stepper motor pulses
         # 12000 = open all the way
@@ -189,7 +191,7 @@ class Sphere(object):
                     sys.exit()
             return
 
-    def VA_Calculate_Shutter_Position(self, light_intensity):
+    def va_calculate_shutter_position(self, light_intensity):
         """ Calculates the required shutter position (in %) to give the desired light intensity (in %).
 	 Uses I = A + B tanh(C s + D), where s is the shutter position. Constants come from fit to photometer data"""
         A = 4.58586028
@@ -209,29 +211,29 @@ class Sphere(object):
         else:
             return s
 
-    def VA_Set_Light_Intensity(self, light_intensity):
+    def va_set_light_intensity(self, light_intensity):
         """ Opens the shutter a specified amount given the input light intensity value.  Assumes 0.0 < Value < 100."""
         print("Set Light Intensity to %f"%light_intensity )
         if light_intensity < 0.0 or light_intensity > 100.0:
             print("Light Intensity value not between 0 and 100. Exiting")
             sys.exit()
 
-        self.VA_Drive_Shutter(12000)
+        self.va_drive_shutter(12000)
         # This opens the shutter all the way
         print("Opening shutter completely for calibration.")
         time.sleep(0.5)
 
-        sp = self.VA_Calculate_Shutter_Position(light_intensity)
+        sp = self.va_calculate_shutter_position(light_intensity)
         # This linearizes the non-linearity of the shutter
         Shutter_Position = int(12000 * sp / 100) - 12000
-        self.VA_Drive_Shutter(Shutter_Position)
+        self.va_drive_shutter(Shutter_Position)
         # This opens the shutter the requested amount
         time.sleep(0.5)
         self.light_intensity=light_intensity
         self.Read_Photodiode()
         return
 
-    def Read_Photodiode(self):
+    def read_photodiode(self):
         """ Reads the photodiode current and returns the value in amps"""
         self.light_socket.send(b"D\r")
         dummy_val = self.light_socket.recv(100)
