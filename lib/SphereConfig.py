ac7bdda5df202236620d5e626906e7f1403cb40d
CCD Lab <ccd@viscacha.physics.ucdavis.edu>
2023-05-31 16:06:20 -0700

    New version of SphereConfig ready for integration in acquisition script.

--- lib/SphereConfig.py
+++ lib/SphereConfig.py
@@ -6,6 +6,8 @@
 
 #This code was largely written by Craig Lage for the Storm data aquisition system. It was edited and repurposed for the Viscacha REB5 system
 # 2023 Daniel Polin
+#
+#Additional edits made by Adam Snyder to adapt for usage outside of a GUI environment.
 
 import math
 import time
@@ -18,186 +20,216 @@ class Sphere(object):
     def __init__(self):
 
         socket.setdefaulttimeout(0.5)# Timeout if no connection after 0.5 seconds
-        #*** Light & photodiode settings ***
-        self.light_IP = '192.168.1.100'
+
+        ## Light & photodiode socket settings
+        self.light_ip = '192.168.1.100'
         self.light_tcp_portnum = 51344
-        self.buffer_size = 1024
+        self.light_buffer_size = 1024
 
-        #*** Variable Aperture settings ***
-        self.VA_TCP_IP = '192.168.1.200'
-        self.VA_TCP_PORT = 4000
-        self.VA_BUFFER_SIZE = 1024
-        accel_value = '15' #Standard acceleration
-        decel_value = '15' #Standard deceleration
-        velocity_value = '1.1' #Standard velocity
-        current_value = '0.2' #Standard current
-        magnification_value = '3' #Standard scale
+        ## Variable aperture socket settings
+        self.aperture_ip = '192.168.1.200'
+        self.aperture_tcp_portnum = 4000
+        self.aperture_buffer_size = 1024
 
-        self.Value_list = [('AC',accel_value), ('MR',magnification_value), ('DE',decel_value), ('VE',velocity_value), ('CC', current_value)]
-        # Sets shutter to proper settings
-        self.Tolerance = 0.05 # Checks that settings are equal to what was requested 
-                         # within this tolerance.
+        ## Variable aperture settings
+        self.setting_dict = {'acceleration' : ('AC', '15'),
+                             'deceleration' : ('DE', '15'),
+                             'velocity' : ('VE', '1.1'),
+                             'current' : ('CC', '0.2'),
+                             'magnification' : ('MR', '3')}
+
+       # Sets shutter to proper settings
+        self.tolerance = 0.05
         self.light_intensity = 0.0
+ 
+        self.initialize_light_socket()
+        self.initialize_aperture_socket()
 
-        return
+    def close_sockets(self):
+        """Close the light and aperture socket."""
+        self.light_socket.close()
+        self.aperture_socket.close()
 
-    def initialize_light_socket(self):
-        """Initializes the light socket over Ethernet."""
-        for NumTries in range(3):
+    def initialize_light_socket(self, num_tries=3):
+        """Initializes the light socket over Ethernet.
+
+        Parameters
+        ----------
+        num_tries : `int`, (optional)
+            Number of times to try to initialize the light socket (the default is 3).
+        """
+        ## Try to initialize the light socket
+        for n in range(num_tries):
             try:
                 self.light_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
-                self.light_socket.connect((self.light_IP, self.light_tcp_portnum))
-                print("Successfully initialized Light Socket\n")
+                self.light_socket.connect((self.light_ip, self.light_tcp_portnum))
                 return
             except Exception as e:
-                print("Failure to initialize light socket. Exception of type %s and args = \n"%type(e).__name__, e.args  )  
                 time.sleep(0.1)
                 continue
-        print("Failed to initialize Light Socket\n")
-        return
-
-    def close_light_socket(self):
-        """ Closes the light socket over Ethernet"""
-        try:
-            self.light_socket.close()
-            print("Successfully closed Light Socket\n")
-            return
-        except Exception as e:
-            print("Failure to close light socket. Exception of type %s and args = \n"%type(e).__name__, e.args)    
-            return
-    
-    def initialize_shutter_socket(self):
-        # Initializes the shutter socket over Ethernet
-        # And sets the parameters (speed, acceleration, etc) to the proper values
-        for NumTries in range(3):
-            try:
-                self.shutter_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.shutter_socket.connect((self.VA_TCP_IP, self.VA_TCP_PORT))
-                for (Name,Value) in self.Value_list:
-                    self.shutter_socket.send(bytes("%s\r"%(Name+Value)))
-                    time.sleep(0.1)
-                    self.shutter_socket.send(bytes("%s\r"%(Name)))
-                    buff=(self.shutter_socket.recv(self.VA_BUFFER_SIZE))
-                    buff=buff.decode()
-                    buff= buff.split('=')[1]
-                    val = float(buff)
-                    print(Name, Value, val)
-                    test_tolerance = abs((val - float(Value)) / float(Value))
-                    if test_tolerance < self.Tolerance:
-                        print("Successfully set parameter "+Name+" to value "+Value+". Measured value = "+str(val))
-                    else:
-                        print("Failed to set parameter "+Name+" to value "+Value)
-                        print("Set-up failed.  Exiting.")
-                        self.shutter_socket.close()
-                        return
-                print("Successfully initialized Shutter Socket\n")
-                return
-            except Exception as e:
-                print("Failure to initialize shutter socket. Exception of type %s and args = \n"%type(e).__name__, e.args)    
-                time.sleep(0.1)
-                continue
-
-        print("Failed to initialize Shutter Socket\n")
-        return
-
-    def close_shutter_socket(self):
-        """ Closes the shutter socket over Ethernet"""
-        try:
-            self.shutter_socket.close()
-            print("Successfully closed Shutter Socket\n")
-            return
-        except Exception as e:
-            print("Failure to close shutter socket. Exception of type %s and args = \n"%type(e).__name__, e.args)    
-            return
-
-    def check_communications(self):
-        # Checks on communications staus
-        self.light_socket_status = False
-        try:
-            test_IP = self.light_socket.getpeername()[0]
-            if test_IP == self.light_IP:
-                self.light_socket_status = True
-        except Exception as e:
-            print("Light socket not communicating. Exception of type %s and args = \n"%type(e).__name__, e.args)    
-            self.light_socket_status = False
-
-        self.shutter_socket_status = False
-        try:
-            test_IP = self.shutter_socket.getpeername()[0]
-            if test_IP == self.VA_TCP_IP:
-                self.shutter_socket_status = True
-        except Exception as e:
-            print("Shutter socket not communicating. Exception of type %s and args = \n"%type(e).__name__, e.args)    
-        self.comm_status = self.light_socket_status and self.shutter_socket_status
-        return
-
-
-    def turn_light_on(self):
-        # Turns the light on
-        print("Turning light on - 4 second delay for stabilization.")
-        self.light_socket.send(bytes("PS2~1\r"))
-        time.sleep(4)
-        self.read_photodiode()
-        return
-
-    def turn_light_off(self):
-        # Turns the light off
-        print("Turning light off - 8 second delay for stabilization.")
-        self.light_socket.send(bytes("PS2~0\r"))
-        time.sleep(8)
-        self.read_photodiode()
-        return
-
-    def final_light_off(self):
-        # Turns the light off at program exit without updating photodiode current
-        print("Turning light off")
-        self.light_socket.send("PS2~0\r")
-        return
-
-    def va_drive_shutter(self, Move_value):
-        # Moves the shutter to a value specified by a Move_value
-        # Move_value is a number of stepper motor pulses
-        # 12000 = open all the way
-        self.shutter_socket.send(b"DL\r")
-        buff=(self.shutter_socket.recv(self.VA_BUFFER_SIZE))
-        buff=buff.decode()
-        buff= buff.split('=')[1]
-        DL_Status = float(buff)
-        if DL_Status != 1.0:
-                print("DL value not equal to 1. Failed shutter move. Exiting.")
-                sys.exit()
-        print("DL_Status = ",DL_Status)
-
-        self.shutter_socket.send(bytes("%s\r"%('DI'+str(Move_value))))
-        self.shutter_socket.send(b"FL\r")
-
-        Move_Success = False
-        for NumTries in range(20):
-                time.sleep(2)
-                self.shutter_socket.send(b"RS\r")
-                Status = self.shutter_socket.recv(self.VA_BUFFER_SIZE)
-                print("Status = ", Status.decode())
-                if Status == b'R\r':
-                        Move_Success = True
-                        break
-        self.shutter_socket.send(b"RV\r")
-        buff=(self.shutter_socket.recv(self.VA_BUFFER_SIZE))
-        buff=buff.decode()
-        buff= buff.split('=')[1]
-        RV_Status = int(buff)
-        if RV_Status != 223:
-                print("RV value not equal to 223. Failed shutter move. Exiting.")
-                sys.exit()
-        if Move_Success:
-                print("Successfully moved shutter as requested")
+        ## Raise exception if out of tries
         else:
-                print("Number of status tests exceeded.  Failed shutter move. Exiting.")
-                sys.exit()
-        return
+            raise e
+   
+    def initialize_aperture_socket(self, num_tries=3):
+        """Initialize the aperture socket over Ethernet.
 
-    def va_calculate_shutter_position(self, light_intensity):
-        """ Calculates the required shutter position (in %) to give the desired light intensity (in %).
-	 Uses I = A + B tanh(C s + D), where s is the shutter position. Constants come from fit to photometer data"""
+        Parameters
+        ----------
+        num_tries: `int`, (optional)
+            Number of times to try to initialize the aperture socket. (the default is 3).
+
+        Raises
+        ------
+        RuntimeError
+            Raised if variable aperture parameter values cannot be set.
+        """
+        ## Try to initialize the aperture socket over Ethernet
+        for n in range(num_tries):
+            try:
+                self.aperture_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.aperture_socket.connect((self.aperture_ip, self.aperture_tcp_portnum))
+                break
+            except Exception as e:
+                time.sleep(0.1)
+                continue
+        ## Raise exception if out of tries
+        else:
+            raise e
+
+        ## Set parameters to the proper values
+        for (name, value) in list(self.setting_dict.values()):
+            self.aperture_socket.send(bytes("%s\r"%(name+value)))
+            time.sleep(0.1)
+            self.aperture_socket.send(bytes("%s\r"%(name)))
+            buff=(self.aperture_socket.recv(self.aperture_buffer_size))
+            buff=buff.decode()
+            buff= buff.split('=')[1]
+            val = float(buff)
+            test_tolerance = abs((val - float(value)) / float(value))
+            if test_tolerance >= self.tolerance:
+                self.aperture_socket.close()
+                raise RuntimeError("Failed to set parameter {0} to value {1}".format(name, value))
+
+    def verify_communications(self):
+        """Verify communication status."""
+
+        light_socket_status = (self.light_socket.getpeername()[0] == self.light_ip)
+        aperture_socket_status = (self.aperture_socket.getpeername()[0] == self.aperture_ip)
+        
+        return (light_socket_status and aperture_socket_status)
+
+    def turn_light_on(self, delay_time=4.0):
+        """Turn on the light.
+
+        Parameters
+        ----------
+        delay_time : `float`
+            Time in seconds to wait for light stabilization.
+        """
+        self.light_socket.send(bytes("PS2~1\r"))
+        time.sleep(delay_time)
+
+    def turn_light_off(self, delay_time=10.0):
+        """Turn off the light.
+
+        Parameters
+        ----------
+        delay_time : `float`
+            Time in seconds to wait for light stabilization. 
+        """
+        self.light_socket.send(bytes("PS2~0\r"))
+        time.sleep(delay_time)
+
+    def drive_aperture(self, move_value, num_waits=20):
+        """Moves the variable aperture to the specified value.
+
+        Parameters
+        ----------
+        move_value : `int`
+            Variable aperture position specified as number of stepper motor pulses.
+        num_waits: `int`
+            Number of timesteps (2 seconds) to wait for move
+        """
+        ## Check DL status
+        self.aperture_socket.send(b"DL\r")
+        buff = self.aperture_socket.recv(self.aperture_buffer_size).decode()
+        dl_status = float(buff.split('=')[1])
+        if dl_status != 1.0:
+            raise RuntimeError("DL value not equal to 1. Failed aperture move.")
+
+        ## Send move command
+        self.aperture_socket.send(bytes("%s\r"%('DI'+str(move_value))))
+        self.aperture_socket.send(b"FL\r")
+
+        ## Wait for move completion
+        for n in range(num_waits):
+            time.sleep(2)
+            self.aperture_socket.send(b"RS\r")
+            status = self.aperture_socket.recv(self.aperture_buffer_size)
+            if status == b'R\r':
+                break
+        else:
+            raise RuntimeError("Move not completed after {0} timesteps.".format(num_waits))    
+
+        ## Check RV status
+        self.aperture_socket.send(b"RV\r")
+        buff = self.aperture_socket.recv(self.aperture_buffer_size).decode()
+        rv_status = int(buff.split('=')[1])
+        if rv_status != 223:
+            raise RuntimeError("RV value not equal to 223. Failed aperture move.")
+
+    def set_light_intensity(self, light_intensity):
+        """ Opens the variable aperture a specified amount given the input light intensity value.
+
+        Parameters
+        ----------
+        light_intensity : `float`
+            Light intensity in percentage (between 0 and 100.)
+        """
+        sp = self.calculate_aperture_position(light_intensity)
+        aperture_position = int(12000 * sp / 100) - 12000
+
+        ## Open aperture all the way
+        self.drive_aperture(12000)
+        time.sleep(0.5)
+
+        ## Open aperture to desired amount
+        self.drive_aperture(aperture_position)
+        time.sleep(0.5)
+        self.light_intensity = light_intensity
+
+    def read_photodiode(self):
+        """ Read the photodiode current.
+
+        Returns
+        -------
+        diode_current : `float`
+            Photodiode current in amps.
+        """
+        self.light_socket.send(b"D\r")
+        dummy_val = self.light_socket.recv(100)
+        time.sleep(0.2)
+        self.light_socket.send(b"D\r")
+        diode_current = float(self.light_socket.recv(100).rstrip('\r'))
+        time.sleep(0.1)
+        
+        return diode_current
+
+    @staticmethod
+    def calculate_aperture_position(light_intensity):
+        """Calculate the required aperture position (in %) for given light intensity (in %)
+
+        Uses I = A + B tanh(C s + D), where s is the aperture position. Constants come from fit to photometer data
+
+        Parameters
+        ----------
+        light_intensity : `float`
+            Light intensity in percentage.
+        """
+        if light_intensity < 0.0 or light_intensity > 100.0:
+            raise ValueError("Light Intensity value {0} not between 0 and 100.")
+
         A = 4.58586028
         B = 4.56817782
         C = 0.06850201
@@ -205,45 +237,8 @@ class Sphere(object):
         Ap = A / (A + B)
         Bp = B / (A + B)
 
-        if light_intensity < 1.0:
-            print("Light intensity not accurate for intensities < 1%.\n")
-        elif light_intensity > 99:
-            print("Light intensity not accurate for intensities >99%.  Opening shutter all the way.\n")
+        if light_intensity > 99:
             return 100.0
- 
-        s = (math.atanh((light_intensity / 100.0 - Ap) / Bp) - D) / C
-        return s
-
-    def va_set_light_intensity(self, light_intensity):
-        """ Opens the shutter a specified amount given the input light intensity value.  Assumes 0.0 < Value < 100."""
-        print("Set Light Intensity to %f"%light_intensity )
-        if light_intensity < 0.0 or light_intensity > 100.0:
-            print("Light Intensity value not between 0 and 100. Exiting")
-            sys.exit()
-
-        self.va_drive_shutter(12000)
-        # This opens the shutter all the way
-        print("Opening shutter completely for calibration.")
-        time.sleep(0.5)
-
-        sp = self.va_calculate_shutter_position(light_intensity)
-        # This linearizes the non-linearity of the shutter
-        Shutter_Position = int(12000 * sp / 100) - 12000
-        self.va_drive_shutter(Shutter_Position)
-        # This opens the shutter the requested amount
-        time.sleep(0.5)
-        self.light_intensity=light_intensity
-        self.read_photodiode()
-        return
-
-    def read_photodiode(self):
-        """ Reads the photodiode current and returns the value in amps"""
-        self.light_socket.send(b"D\r")
-        dummy_val = self.light_socket.recv(100)
-        time.sleep(0.2)
-        self.light_socket.send(b"D\r")
-        self.diode_current = self.light_socket.recv(100)
-        diode_out=(str(self.diode_current).rstrip('\r'))
-        time.sleep(0.1)
-        print("Light Intensity "+diode_out+"Amperes")
-        return diode_out
+        else:
+            s = (math.atanh((light_intensity / 100.0 - Ap) / Bp) - D) / C
+            return s
