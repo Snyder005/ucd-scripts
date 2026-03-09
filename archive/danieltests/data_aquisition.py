a57698c63497df98ad7a41227c81d99dce751a3d
CCD Lab <ccd@viscacha.physics.ucdavis.edu>
2026-03-08 17:31:28 -0700

    Moved archived code and consolidated eotest config file directories.

--- archive/danieltests/data_aquisition.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import time
-
-subprocess.run('ccs-script lib/ucd-data.py examples/spot.cfg',check=True, shell=True)
-
-time.sleep(1)
-
-subprocess.run('lib/python3 autosort.py',check=True, shell=True)
