a43c5fb5ab684a7444d5684475eb1119687a9796
CCD Lab <ccd@viscacha.physics.ucdavis.edu>
2026-03-08 18:26:13 -0700

    Removed unused jython modules.

--- scripts/ucd-data.py
+++ scripts/ucd-data.py
@@ -14,10 +14,7 @@ from org.lsst.ccs.scripting import CCS
 
 from ccs import aliases
 from ccs import proxies
-from ccs import versions
-from ccs import configs
 import config
-import BackBiasCheck # in future maybe rename
 
 # Temporary work around for problems with CCS responsiveness
 CCS.setDefaultTimeout(Duration.ofSeconds(30))
@@ -27,21 +24,7 @@ class WarningFilter(object):
     def filter(self, log_record): 
         return log_record.levelno != logging.WARNING
 
-def bss_monitor(stop_event, outfile):
-
-    bss = BackBiasCheck.BackBias()
-    bss.check_connections()
-
-    with open(outfile, 'a') as f:
-        while not stop_event.is_set():
-            vss = float(bss.read_BSS())
-            iss = float(bss.read_ISS())
-            now = datetime.datetime.now()
-            output = "{0}, {1:.4f}, {2:.4f}\n".format(now, vss, iss)
-            f.write(output)
-            time.sleep(0.1)
-
-def main(cfgfile, run=None, log_bss=False):
+def main(cfgfile):
 
     ## Set up logging
     logger = logging.getLogger()
@@ -76,40 +59,17 @@ def main(cfgfile, run=None, log_bss=False):
     globalfile_handler.setFormatter(log_format)
     logger.addHandler(globalfile_handler)
 
-    ## Write config versions to cwd
-    if run:
-        fp = CCS.attachProxy('ucd-fp')
-        time.sleep(10.0)
-        versions.write_versions(fp)
-        configs.write_config(fp, ['Sequencer', 'Rafts'])
-
-    ## Set up background thread
-    bss_logfile = os.path.join(rootdir, today, '{0}_bss.log'.format(today))
-    stop_event = threading.Event()
-    background_thread = threading.Thread(target=bss_monitor, args=(stop_event, bss_logfile))
-
-    ## Optionally start BSS monitor
-    if log_bss:
-        background_thread.start()
-
     ## Parse config file and execute data acquisition
     try:
         cfg = config.parseConfig(cfgfile)
-        config.execute(cfg, {"run" : run})
+        config.execute(cfg, {"run" : None}) # is the run object needed?
     except (JException, Exception):
         logger.exception("Fatal error occurred in data acquisition.")
-
-    ## Stop BSS monitoring if it is running
-    if background_thread.is_alive():
-        stop_event.set()
-        background_thread.join()
 
 if __name__ == '__main__':
 
     parser = ArgumentParser(sys.argv[0], add_help=False)
     parser.add_argument('cfgfile', type=str)
-    parser.add_argument('--run', type=str, default=None)
-    parser.add_argument('--log-bss', action='store_true')
 
     args = parser.parse_args()
-    main(args.cfgfile, args.run, args.log_bss)
+    main(args.cfgfile)
