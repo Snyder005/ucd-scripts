edd24572b33cc62d6fae85321a7a66fc49a8e390
CCD Lab <ccd@viscacha.physics.ucdavis.edu>
2026-03-24 13:29:24 -0700

    Merge PowerControl into power submodule.

--- lib/ccs/power.py
+++ lib/ccs/power.py
@@ -9,6 +9,100 @@ from ccs.data import DeviceException
 
 __all__ = ['BK9130BDevice', 'BK9184Device', 'BK1697BDevice']
 
+class PowerControl(object):
+    """Power supply channel controller.
+
+    Parameters
+    ----------
+    name : `str`
+        Power supply channel name.
+    devc : `PowerDevice`
+        A power supply device interface.
+    hw_chan : `int`
+        Power supply channel number.
+    op_voltage : `float`
+        Power supply channel operating voltage.
+    op_current : `float`, optional
+        Power supply channel operating current.
+
+    Raises
+    ------
+    PowerException
+        Raised if the hardware channel number is invalid.
+    """
+
+    def __init__(self, name, devc, hw_chan, op_voltage, op_current):
+        self.name = name
+        self.devc = devc
+        if (hw_chan < self.devc.MIN_CHAN) or (hw_chan > self.devc.MAX_CHAN):
+            raise PowerException('HW channel number is invalid: {0}'.format(hw_chan))
+        else:
+            self.hw_chan = hw_chan
+        self.op_voltage = op_voltage
+        self.op_current = op_current
+
+    def get_state(self):
+        """Get state of the power supply channel."""
+        try:
+            state = self.read_output()
+            voltage = 0.0 if state == 'NC' else self.read_voltage()
+            current = 0.0 if state == 'NC' else self.read_current()
+        except PowerException:
+            state = 'NC'
+            voltage = 0.0
+            current = 0.0
+
+        return self.name, state, voltage, current
+
+    def read_voltage(self):
+        """Read the power supply channel voltage.
+
+        Returns
+        -------
+        voltage : `float`
+            Power supply channel voltage.
+        """
+        return self.devc.read_voltage(self.hw_chan)
+
+    def write_voltage(self):
+        """Write the power supply channel setpoint voltage."""
+        self.devc.write_voltage(self.op_voltage, self.hw_chan)
+    
+    def read_current(self):
+        """Read the power supply channel current.
+
+        Returns
+        -------
+        current : `float`
+            Power supply channel current.
+        """
+        return self.devc.read_current(self.hw_chan)
+
+    def write_current(self):
+        """Write the power supply channel current limit."""
+        self.devc.write_current(self.op_current, self.hw_chan)
+
+    def read_output(self):
+        """Read the power supply channel output.
+
+        Returns
+        -------
+        state : {'NC', 'ON', 'OFF'}
+            Power supply channel output state.
+        """
+        return self.devc.read_output(self.hw_chan)
+
+    def write_output(self, state):
+        self.devc.write_output(state, self.hw_chan)
+
+    def power_on(self):
+        self.write_voltage()
+        self.write_current()
+        self.write_output('ON')
+
+    def power_off(self):
+        self.write_output('OFF')
+
 class PowerDevice(SerialDevice):
 
     def is_connected(self):
--- lib/ccs/ucd_power.py
+++ lib/ccs/ucd_power.py
@@ -2,7 +2,7 @@
 # To Do:
 #  * Write a useful publish state function
 from ccs.power import BK9184Device, BK9130BDevice, BK1697BDevice
-from ccs.power_control import PowerControl
+from ccs.power import PowerControl
 
 from time import sleep
 
@@ -17,15 +17,15 @@ class UCDPower(object):
         self.bk1697b = BK1697BDevice('/dev/serial/by-path/pci-0000:00:14.0-usb-0:12.1.4:1.0')
 
         # Define controls
-        self.reb_controls = {'Analog' : PowerControl('Analog', self.bk9130b1, 1, 8.0),
-                             'Heater' : PowerControl('Heater', self.bk9130b1, 2, 12.0),
-                             'Digital' : PowerControl('Digital', self.bk9130b1, 3, 5.0),
-                             'ClockLow' : PowerControl('ClockLow', self.bk9130b2, 1, 15.0),
-                             'ClockHigh' : PowerControl('ClockHigh', self.bk9130b2, 2, 15.0),
-                             'OD' : PowerControl('OD', self.bk1697b, 0, 38.0)}
+        self.reb_controls = {'Analog' : PowerControl('Analog', self.bk9130b1, 1, 8.0, 0.75),
+                             'Heater' : PowerControl('Heater', self.bk9130b1, 2, 12.0, 1.0),
+                             'Digital' : PowerControl('Digital', self.bk9130b1, 3, 5.0, 1.5),
+                             'ClockLow' : PowerControl('ClockLow', self.bk9130b2, 1, 15.0, 0.1),
+                             'ClockHigh' : PowerControl('ClockHigh', self.bk9130b2, 2, 15.0, 0.1),
+                             'OD' : PowerControl('OD', self.bk1697b, 0, 38.0, 0.05)}
 
-        self.hvbias_control = PowerControl('HVBias', self.bk9184, 0, 50.0, current=0.001)
-        self.otm_control = PowerControl('OTM', self.bk9130b2, 3, 5.0)
+        self.hvbias_control = PowerControl('HVBias', self.bk9184, 0, 50.0, 0.001)
+        self.otm_control = PowerControl('OTM', self.bk9130b2, 3, 5.0, 0.5)
 
     def power_on(self):
 
@@ -38,8 +38,8 @@ class UCDPower(object):
                 sleep(20)
 
                 # Turn on Clk High, Clk Low, and Analog voltages
-                self.reb_controls['ClkHigh'].power_on()
-                self.reb_controls['ClkLow'].power_on()
+                self.reb_controls['ClockHigh'].power_on()
+                self.reb_controls['ClockLow'].power_on()
                 self.reb_controls['Analog'].power_on()
                 sleep(20)
 
@@ -57,12 +57,15 @@ class UCDPower(object):
             sleep(20)
 
             self.reb_controls['Analog'].power_off()
-            self.reb_controls['ClkLow'].power_off()
-            self.reb_controls['ClkHigh'].power_off()
+            self.reb_controls['ClockLow'].power_off()
+            self.reb_controls['ClockHigh'].power_off()
             sleep(20)
     
             self.reb_controls['Heater'].power_off()
             self.reb_controls['Digital'].power_off()
+            sleep(4)
+        finally:
+            self.publish_state()
             
     def hvbias_on(self):
         if not self.is_power_on():
@@ -91,27 +94,39 @@ class UCDPower(object):
         finally:
             self.publish_state()
 
+    def otm_on(self):
+        try:
+            self.otm_control.power_on()
+        finally:
+            self.publish_state()
+
+    def otm_off(self):
+        try:
+            self.otm_control.power_off()
+        finally:
+            self.publish_state()
+
     def publish_state(self):
         """Read state of power supplies and publish."""
         
         # REB control
-        for control in self.reb_control:
-            name, state, voltage, _ = control.get_state()
-            print('{0}: State = {1}, voltage = {2:.3f}'.format(name, state, voltage))
+        for control in self.reb_controls.values():
+            name, state, voltage, current = control.get_state()
+            print('{0}: State = {1}, voltage = {2:.3f} V, current = {3:.3f} A'.format(name, state, voltage, current))
 
         # HVBias control
-        name, state, voltage, _ = hvbias_control.get_state()
-        print('{0}: State = {1}, voltage = {2:.3f}'.format(name, state, voltage))
+        name, state, voltage, current = self.hvbias_control.get_state()
+        print('{0}: State = {1}, voltage = {2:.3f} V, current = {3:.3f} A'.format(name, state, voltage, current))
 
         # OTM control
-        name, state, voltage, _ = otm_control.get_state()
-        print('{0}: State = {1}, voltage = {2:.3f}'.format(name, state, voltage))
+        name, state, voltage, current = self.otm_control.get_state()
+        print('{0}: State = {1}, voltage = {2:.3f} V, current = {3:.3f} A'.format(name, state, voltage, current))
 
     def is_power_on(self):
 
         is_on = True
 
-        for control in self.reb_control:
+        for control in self.reb_controls.values():
             state = control.read_output()
             if state != 'ON':
                 is_on = False
